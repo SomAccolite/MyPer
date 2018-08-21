f460839fba94adb7ac237d3862d954e985ca3bfc
SomAccolite <somji.trivedi@accolitelabs.com>
2018-08-21 13:48:14 +0530

    First commit.

---
+++ src/main/java/com/crossover/techtrial/model/InitialProcessStatus.java
@@ -0,0 +1,12 @@
+package com.crossover.techtrial.model;
+
+/**
+ * Available statuses for initial process.
+ *
+ * @author Som
+ */
+public enum InitialProcessStatus {
+    STARTED,
+    FINISHED,
+    FAILED
+}
